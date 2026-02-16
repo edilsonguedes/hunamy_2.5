c22b46276f2f1b417108de120e8ecce1f9c85f77
Edilson Guedes <edilsonguedesads@gmail.com>
2026-02-16 20:14:33 -0300

    style: adjust logo size in header and footer for better proportions

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
                                 alt="Hunamy"
                                 width={140}
                                 height={40}
-                                className="h-8 w-auto object-contain brightness-0 invert"
+                                className="h-6 w-auto object-contain brightness-0 invert"
                             />
                         </Link>
                     </div>
